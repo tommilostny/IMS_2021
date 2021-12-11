549018ac3db9db37171dd70e5d6d9d08500fe97b
tommilostny <tommilostny@live.com>
2021-12-11 16:44:46 +0100

    Added yearly order rate increase.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <simlib.h>
+#include <vector>
 
 #define YEARS 10
 #define HOURS_IN_YEAR 8760
@@ -107,16 +108,22 @@ class YearTracker : public Event
 {
 public:
     uint16_t year;
-    YearTracker(uint16_t startYear) : year(startYear) {}
+    std::vector<Producer*> producers;
+    std::vector<Consumer*> consumers;
+
+    YearTracker(uint16_t startYear, std::vector<Producer*> producers, std::vector<Consumer*> consumers)
+        : year(startYear), producers(producers), consumers(consumers)
+    {
+    }
 
     void Behavior()
     {
         std::cout << year << " -> ";
         year++;
-        //orderRate += 0.1;
         std::cout << year << std::endl;
 
-        // for each producer, check year for planned factory opening, update production rate
+        for (auto consumer : consumers)
+            consumer->AddToOrderRate(0.1);
 
         Activate(Time + HOURS_IN_YEAR);
     }
@@ -128,21 +135,30 @@ int main()
     Init(0, HOURS_IN_YEAR * YEARS);
     auto globalStorage = new Storage();
 
-    // Create producers
-    auto tsmc = new Producer(12500000, 600, "TSMC", globalStorage);
-    auto samsung = new Producer(3060000 * 12, 300, "Samsung", globalStorage);
-    //auto Intel
-    //auto others
+    std::vector<Producer*> producers = 
+    {
+        new Producer(12500000, 600, "TSMC", globalStorage),
+        new Producer(3060000 * 12, 300, "Samsung", globalStorage)
+        //auto Intel
+        //auto others
+    };
 
-    tsmc->Activate();
-    samsung->Activate();
+    std::vector<Consumer*> consumers = 
+    {
+        new Consumer(15000000000, "Automotive", globalStorage)
+    };
 
-    // Create consumers
-    (new Consumer(18500000000, "Automotive", globalStorage))->Activate(Time + HOURS_IN_DAY);
+    for (auto producer : producers)
+        producer->Activate();
+    
+    for (auto consumer : consumers)
+        consumer->Activate(Time + HOURS_IN_DAY);
 
     // Create year tracker that updates yearly production and order rates
-    (new YearTracker(2020))->Activate(Time + HOURS_IN_YEAR);
+    (new YearTracker(2020, producers, consumers))->Activate(Time + HOURS_IN_YEAR);
 
     Run();
+
+    delete globalStorage;
     return 0;
 }
